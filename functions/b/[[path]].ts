2196c095a79033e0ecce8d7d13f407067e81a2f8
ronyopq <ronyopq@gmail.com>
2026-03-11 04:42:59 +0600

    Remove unused Pages functions and rely on _worker proxy

--- functions/b/[[path]].ts
+++
@@ -1,8 +0,0 @@
-const WORKER_ORIGIN = "https://personal-library.ronybd.workers.dev";
-
-export const onRequest: PagesFunction = async (context) => {
-  const incomingUrl = new URL(context.request.url);
-  const targetUrl = new URL(`${WORKER_ORIGIN}${incomingUrl.pathname}${incomingUrl.search}`);
-
-  return fetch(new Request(targetUrl.toString(), context.request));
-};
